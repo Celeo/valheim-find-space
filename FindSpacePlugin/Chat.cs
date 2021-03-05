19d870ce497b3d948324923a7fe03b12e277d3f5
Celeo <mattboulanger@fastmail.com>
2021-03-04 23:07:22 -0800

    Init

---
+++ FindSpacePlugin/Chat.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindSpace
+{
+    [HarmonyPatch(typeof(Chat), "InputText")]
+    public class HookChatInputText
+    {
+        static bool Prefix(Chat __instance)
+        {
+            string text = __instance.m_input.text;
+            if (text == "/findspace")
+            {
+                SearchNearbyForSpace();
+                return false;
+            }
+            return true;
+        }
+
+        static void SearchNearbyForSpace()
+        {
+            var pieces = new List<Piece>();
+            Piece.GetAllPiecesInRadius(Player.m_localPlayer.transform.position, 10f, pieces);
+            pieces
+                .Where(p => p.GetComponent<Container>())
+                .Where(p => p.GetComponent<Container>().GetInventory().GetEmptySlots() > 0)
+                .ToList()
+                .ForEach(p => HighlightPiece(p));
+        }
+
+        static void HighlightPiece(Piece piece)
+        {
+            var component = piece.GetComponent<WearNTear>();
+            if (component)
+            {
+                component.Highlight();
+            }
+        }
+    }
+}
---
+++ FindSpacePlugin/FindSpacePlugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace FindSpace
+{
+    [BepInPlugin("com.celeo.valheim.findspace", "Find Space", "1.0.0.0")]
+    public class FindSpacePlugin : BaseUnityPlugin
+    {
+        private static Harmony harmonyInstance;
+
+        void Awake()
+        {
+            UnityEngine.Debug.Log("FindSpace plugin initialized"); harmonyInstance = Harmony.CreateAndPatchAll(
+                 Assembly.GetExecutingAssembly(),
+                 "com.celeo.valheim.findspace"
+                );
+        }
+
+        void OnDestroy()
+        {
+            harmonyInstance.UnpatchSelf();
+        }
+    }
+}
